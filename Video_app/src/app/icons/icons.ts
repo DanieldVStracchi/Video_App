fa522ce6c476a1e4e6feb56154e19cd47e338f6a
Naiiden <hugo.plissonneau@viacesi.fr>
2026-06-24 17:16:47 +0200

    remove class

--- Video_app/src/app/icons/icons.ts
+++ Video_app/src/app/icons/icons.ts
@@ -23,9 +23,9 @@ export class Icons {
   defineSvg(icon: string, color: string): string {
     switch (icon) {
       case 'delete':
-        return `<svg xmlns="http://www.w3.org/2000/svg" width="${this.width}" height="${this.width}" viewBox="0 0 24 24" fill="none" stroke="${this.color}" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x-icon lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>`
+        return `<svg xmlns="http://www.w3.org/2000/svg" width="${this.width}" height="${this.width}" viewBox="0 0 24 24" fill="none" stroke="${this.color}" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>`
       default:
-        return `<svg xmlns="http://www.w3.org/2000/svg" width="${this.width}" height="${this.width}" viewBox="0 0 24 24" fill="none" stroke="${this.color}" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search-icon lucide-search"><path d="m21 21-4.34-4.34"/><circle cx="11" cy="11" r="8"/></svg>`
+        return `<svg xmlns="http://www.w3.org/2000/svg" width="${this.width}" height="${this.width}" viewBox="0 0 24 24" fill="none" stroke="${this.color}" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m21 21-4.34-4.34"/><circle cx="11" cy="11" r="8"/></svg>`
     }
   }
 
